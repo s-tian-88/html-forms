e6e62936cbbe300a79cccb7ccc66e39a90b66176
Sergei Tian <s.tian.spb@gmail.com>
2024-02-27 20:33:39 +0300

    added e2e tests

---
+++ e2e/popover-button.test.js
@@ -0,0 +1,38 @@
+import puppeteer from 'puppeteer';
+
+describe('start page and show button', () => {
+
+  let browser;
+  let page;
+
+  beforeEach (async () => {
+
+    browser = await puppeteer.launch({
+      headless: false,
+      slowmo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+
+  });
+
+  test ('start page', async () => {
+
+    await page.goto('http://localhost:9000');
+    await page.waitForSelector('body');
+
+  });
+
+  test ('show button', async () => {
+
+    await page.goto('http://localhost:9000');
+    await page.waitForSelector('.popover-button');
+
+  });
+
+  afterEach (async () => {
+    await browser.close();
+  });
+
+});
---
+++ e2e/popover-widtet.test.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+describe('poipover widget', () => {
+
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+
+    });
+
+    page = await browser.newPage();
+
+  });
+
+  test('popover-widget', async () => {
+
+    await page.goto('http://localhost:9000');
+
+    const buttonElement = await page.$('button');
+
+    await buttonElement.click();
+
+    await page.waitForSelector('.popover');
+
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+
+});
